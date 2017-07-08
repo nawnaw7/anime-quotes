4be6cc2f08dfb1f91bb0cb701d6e14fc2555d9b6
nawnaw <sirbu.ioana90@gmail.com>
2017-07-08 12:07:25 +0200

    Add files
    the source files for this project

---
+++ script.js
@@ -0,0 +1,479 @@
+
+var anime_quotes = [];
+
+anime_quotes[0] = {
+    "qnumber": 1,
+    "qtext": "In manga, there's always a character who gets a sudden boost of power, right? Readers always call it a 'convenient power up', but it actually does happen in the real world, as well. We Humans only truly show our mettle when we're pushed into a corner.",
+    "qauthor": "Shinobu Takamiya",
+    "qanime": "Beelzebub"
+}
+
+anime_quotes[1] = {
+    "qnumber": 2,
+    "qtext": "Once you taste poison, you might as well finish the meal. If it comes to that you would make as fine a dish as any.",
+    "qauthor": "Horo",
+    "qanime": "Spice and Wolf"
+}
+
+anime_quotes[2] = {
+    "qnumber": 3,
+    "qtext": "There are many types of monsters that scare me: Monsters who cause troubles without showing themselves, monsters who abduct children, monsters who devour dreams, monsters who suck blood... and then, monsters who tell nothing but lies. Lying monsters are a real nuisance: They are much more cunning than others: They pose as humans even though they have no understanding of the human heart; they eat even though they've never experienced hunger; they study even though they have no interest in academics; they seek friendship even though they do not know how to love. If I were to encounter such monsters, I would likely be eaten by them... because in truth, I am that monster.",
+    "qauthor": "L",
+    "qanime": "Death Note"
+}
+
+anime_quotes[3] = {
+    "qnumber": 4,
+    "qtext": "Life is basically like a soap bubble. It rides on the wind, flying here and there...And before you realize it, pop! It's gone. When it's about to disappear, you think that you could've flown a bit higher. But by the time, it's already too late.",
+    "qauthor": "Sakata Gintoki",
+    "qanime": "Gintama"
+}
+
+anime_quotes[4] = {
+    "qnumber": 5,
+    "qtext": "Getting dumped always makes a man stronger, but then again, men aren't meant to pursue happiness...",
+    "qauthor": "Jiraiya",
+    "qanime": "Naruto Shippuden"
+}
+
+anime_quotes[5] = {
+    "qnumber": 6,
+    "qtext": "Religion, ideology, resources, land, spite, love or just because...No matter how pathetic the reason, it's enough to start war. War will never cease to exist...reasons can be thought up after the fact...Human nature pursues strife.",
+    "qauthor": "Pain",
+    "qanime": "Naruto Shippuden"
+}
+
+anime_quotes[6] = {
+    "qnumber": 7,
+    "qtext": "The opposite of love isn't hate, it's apathy.",
+    "qauthor": "Kyoko Hoin",
+    "qanime": "Kodomo no Jikan"
+}
+
+anime_quotes[7] = {
+    "qnumber": 8,
+    "qtext": "People are like dice, a certain Frenchman said that. You throw yourself in the direction of your own choosing. People are free because they can do that. Everyone's circumstances are different, but no matter how small the choice, at the very least, you can throw yourself. It's not chance or fate. It's the choice you made.",
+    "qauthor": "Rokuro Okajima",
+    "qanime": "Black Lagoon"
+}
+
+anime_quotes[8] = {
+    "qnumber": 9,
+    "qtext": "Pirates are evil? The Marines are righteous? These terms have always changed throughout the course of history! Kids who have never seen peace and kids who have never seen war have different values! Those who stand at the top determine what's wrong and what's right! This very place is neutral ground! Justice will prevail, you say? But of course it will! Whoever wins this war becomes justice!",
+    "qauthor": "Donquixote Doflamingo",
+    "qanime": "One Piece"
+}
+
+anime_quotes[9] = {
+    "qnumber": 10,
+    "qtext": "Only hope can give rise to the emotion we call despair. But it is nearly impossible for a man to try to live without hope, so I guess that leaves Man no choice but to walk around with despair as his companion.",
+    "qauthor": "Kagetoki Kariya",
+    "qanime": "Samurai Champloo"
+}
+
+anime_quotes[10] = {
+    "qnumber": 11,
+    "qtext": "Man always thinks about the past before he dies, as if he were frantically searching for proof that he truly lived.",
+    "qauthor": "Jet Black",
+    "qanime": "Cowboy Bebop"
+}
+
+anime_quotes[11] = {
+    "qnumber": 12,
+    "qtext": "There's no hope on the battlefield. It has nothing but unspeakable despair. Just a crime we call victory, paid for by the pain of the defeated. Yet humanity has never recognized this truth. And the reason for that is, in every era, a dazzling hero has blinded people with their legends and prevented them from seeing the evil of bloodshed. The true nature of humanity has not advanced a step beyond the Stone Age.",
+    "qauthor": "Kiritsugu Emiya",
+    "qanime": "Fate/Zero"
+}
+
+anime_quotes[12] = {
+    "qnumber": 13,
+    "qtext": "The world is not beautiful; and that, in a way, lends it a sort of beauty.",
+    "qauthor": "Kino",
+    "qanime": "Kino no Tabi"
+}
+
+anime_quotes[13] = {
+    "qnumber": 14,
+    "qtext": "If you're not remembered, then you never existed.",
+    "qauthor": "Iwakura Lain",
+    "qanime": "Serial Experiments Lain"
+}
+
+anime_quotes[14] = {
+    "qnumber": 15,
+    "qtext": "Man fears the darkness, and so he scrapes away at the edges of it with fire.",
+    "qauthor": "Rei Ayanami",
+    "qanime": "Neon Genesis Evangelion"
+}
+
+anime_quotes[15] = {
+    "qnumber": 16,
+    "qtext": "In a world like that, there'd be no need for love anymore either. If everyone were perfect, there would be no need to look out for others. Sympathy or love is needed because people are imperfect. A perfect human cannot love anyone.",
+    "qauthor": "Keima Katsuragi",
+    "qanime": "The World God Only Knows"
+}
+
+anime_quotes[16] = {
+    "qnumber": 17,
+    "qtext": "There are only two paths you can choose. You can sit quietly and be selected out of this world, or you can adapt and change!",
+    "qauthor": "Gai Tsutsugami",
+    "qanime": "Guilty Crown"
+}
+
+anime_quotes[17] = {
+    "qnumber": 18,
+    "qtext": "People SHOULD be doubted. Many people misunderstand this concept. Doubting people is just a part of getting to know them. What many people call 'trust' is really just giving up on trying to understand others, and that very act is far worse than doubting. It is actually 'apathy'.",
+    "qauthor": "Shinichi Akiyama",
+    "qanime": "Liar Game"
+}
+
+anime_quotes[18] = {
+    "qnumber": 19,
+    "qtext": "The only way to truly escape the mundane is for you to constantly be evolving. Whether you choose to aim high, or aim low. Enjoy each day for what it is.",
+    "qauthor": "Orihara Izaya",
+    "qanime": "Durarara!!"
+}
+
+anime_quotes[19] = {
+    "qnumber": 20,
+    "qtext": "Doctor Tenma. For you all lives are created equal, that's why I came back to life. But you've finally come to realize it now, haven't you? Only one thing is equal for all, and that is death.",
+    "qauthor": "Johan Liebert",
+    "qanime": "Monster"
+}
+
+anime_quotes[20] = {
+    "qnumber": 21,
+    "qtext": "I don't believe in god. There's no proof he exists. In a world where there isn't even proof of the future, the past exists. Even if it's tainted with misunderstandings and delusions, if the people themselves believe in it, the past is the truth to them. And, if you base your actions or your life around it, in a way, it's a type of god itself.",
+    "qauthor": "Orihara Izaya",
+    "qanime": "Durarara!!"
+}
+
+anime_quotes[21] = {
+    "qnumber": 22,
+    "qtext": "Real power is forged from the desire smoldering in the heart. It fans into a flame and begins to burn with an intensity scarcely imagined to be possible.",
+    "qauthor": "Orochimaru",
+    "qanime": "Naruto"
+}
+
+anime_quotes[22] = {
+    "qnumber": 23,
+    "qtext": "A wound that'd make an ordinary man unconscious...I won't lose to it. A wound that would kill an ordinary person...I won't lose to it! To face one who is extraordinary, Hawk Eyes...I can't allow myself to be ordinary!",
+    "qauthor": "Roronoa Zoro",
+    "qanime": "One Piece"
+}
+
+anime_quotes[23] = {
+    "qnumber": 24,
+    "qtext": "If I get reincarnated... . I wanna become a clam.",
+    "qauthor": "Monkey D. Luffy",
+    "qanime": "One Piece"
+}
+
+anime_quotes[24] = {
+    "qnumber": 25,
+    "qtext": "I've set myself to become the King of the Pirates...and if I die trying...then at least I tried!",
+    "qauthor": "Monkey D. Luffy",
+    "qanime": "One Piece"
+}
+
+anime_quotes[25] = {
+    "qnumber": 26,
+    "qtext": "It's not like thanks are something I can eat.",
+    "qauthor": "Monkey D. Luffy",
+    "qanime": "One Piece"
+}
+
+anime_quotes[26] = {
+    "qnumber": 27,
+    "qtext": "Listen up...You can pour drinks on me, you can throw food at me...You can even spit on me. I'll just laugh that stuff off. But...Good reason or not... Nobody hurts a friend of mine!!!",
+    "qauthor": "Shanks",
+    "qanime": "One Piece"
+}
+
+anime_quotes[27] = {
+    "qnumber": 28,
+    "qtext": "The letters I've written before are all blackmail to people. I'm not sure if I know how to write a real letter.",
+    "qauthor": "Killua Zoldyck",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[28] = {
+    "qnumber": 29,
+    "qtext": "They were traps? I thought they were attractions.",
+    "qauthor": "Killua Zoldyck",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[29] = {
+    "qnumber": 30,
+    "qtext": "I do not fear death. I fear only that my rage will fade over time.",
+    "qauthor": "Kurapika",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[30] = {
+    "qnumber": 31,
+    "qtext": "If you want to get to know someone, find out what makes them angry.",
+    "qauthor": "Gon Freecss",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[31] = {
+    "qnumber": 32,
+    "qtext": "That morning's dawn was painted a beautiful scarlet, the likes of which I had never seen before. Like the color of Erza's hair, it was warm and passionate...If she could only lift her face a beautiful sky would be spread out before her.",
+    "qauthor": "Lucy Heartfilia",
+    "qanime": "Fairy Tail"
+}
+
+anime_quotes[32] = {
+    "qnumber": 33,
+    "qtext": "There are people in this world that enjoy being alone, but there isn't a single person who can bear solitude.",
+    "qauthor": "Makarov Dreyar",
+    "qanime": "Fairy Tail"
+}
+
+anime_quotes[33] = {
+    "qnumber": 34,
+    "qtext": "It is always sad to part with those whom you love but your companions will help you bear that sadness.",
+    "qauthor": "Erza Scarlet",
+    "qanime": "Fairy Tail"
+}
+
+anime_quotes[34] = {
+    "qnumber": 35,
+    "qtext": "Why do women have butts on their chests?",
+    "qauthor": "Son Goku",
+    "qanime": "DragonBall Z"
+}
+
+anime_quotes[35] = {
+    "qnumber": 36,
+    "qtext": "The ocean is so salty because everyone pees in it.",
+    "qauthor": "Son Goku",
+    "qanime": "DragonBall Z"
+}
+
+anime_quotes[36] = {
+    "qnumber": 37,
+    "qtext": "Goku! I'd rather die than fuse with you! But Vegeta, we're already dead!",
+    "qauthor": "Goku and Vegeta",
+    "qanime": "DragonBall Z"
+}
+
+anime_quotes[37] = {
+    "qnumber": 38,
+    "qtext": "I'm not a nerd, I'm a specialist.",
+    "qauthor": "Sagara Sousuke",
+    "qanime": "Full Metal Panic!"
+}
+
+anime_quotes[38] = {
+    "qnumber": 39,
+    "qtext": "We just witnessed a classic example of what I call miss directed rage, I believe the technical term is being an ass.",
+    "qauthor": "Sohma Shigure",
+    "qanime": "Fruits Basket"
+}
+
+anime_quotes[39] = {
+    "qnumber": 40,
+    "qtext": "It's amazing how much you kiss your own ass.",
+    "qauthor": "Kyon",
+    "qanime": "The Melancholy of Haruhi Suzumiya"
+}
+
+anime_quotes[40] = {
+    "qnumber": 41,
+    "qtext": "Celebrating Mr. Jesus's birthday(Christmas) was fun, why don't we celebrate Buddha's and Muhammad's birthday next time?",
+    "qauthor": "Suzumiya Haruhi",
+    "qanime": "The Melancholy of Haruhi Suzumiya"
+}
+
+anime_quotes[41] = {
+    "qnumber": 42,
+    "qtext": "All men are jealous and stupid, and all women are stupid to be happy about it, idiots are everywhere you look...",
+    "qauthor": "Horo",
+    "qanime": "Spice and Wolf"
+}
+
+anime_quotes[42] = {
+    "qnumber": 43,
+    "qtext": "Yes I've used a condom before. They can store up to 1 liter of water.",
+    "qauthor": "Sagara Sousuke",
+    "qanime": "Full Metal Panic!"
+}
+
+anime_quotes[43] = {
+    "qnumber": 44,
+    "qtext": "Sorry I'm late. I got lost on the road of life.",
+    "qauthor": "Hatake Kakashi",
+    "qanime": "Naruto"
+}
+
+anime_quotes[44] = {
+    "qnumber": 45,
+    "qtext": "It takes a mere second for treasure to turn to trash.",
+    "qauthor": "Hisoka",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[45] = {
+    "qnumber": 46,
+    "qtext": "You should enjoy the little detours to the fullest. Because that's where you'll find the things more important than what you want.",
+    "qauthor": "Ging Freecss",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[46] = {
+    "qnumber": 47,
+    "qtext": "Human potential for evolution is limitless.",
+    "qauthor": "Netero",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[47] = {
+    "qnumber": 48,
+    "qtext": "When I say it doesn't hurt me, that means I can bear it.",
+    "qauthor": "Killua Zoldyck",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[48] = {
+    "qnumber": 49,
+    "qtext": "People only find me interesting because they can't tell whether I'm serious or not.",
+    "qauthor": "Killua Zoldyck",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[49] = {
+    "qnumber": 50,
+    "qtext": "Right now, I'm letting you live. And I'll continue to keep you alive... until you've grown enough to become worth killing.",
+    "qauthor": "Hisoka",
+    "qanime": "Hunter x Hunter"
+}
+
+anime_quotes[50] = {
+    "qnumber": 51,
+    "qtext": "I'm not rude... I just wasn't taught to politely pretend to be nice to people I can't stand.",
+    "qauthor": "Orihara Izaya",
+    "qanime": "Durarara!!"
+}
+
+anime_quotes[51] = {
+    "qnumber": 52,
+    "qtext": "You think you're special? You're not. Everyone lies, everyone hides things... Nobody makes it through this life being completely honest.",
+    "qauthor": "Origara Izaya",
+    "qanime": "Durarara!!"
+}
+
+anime_quotes[52] = {
+    "qnumber": 53,
+    "qtext": "After everything I've faced.... Having a future with you snatched away from me ...is definitely the most terrifying thing I've ever had to endure....",
+    "qauthor": "Gajeel Redfox",
+    "qanime": "Fairy Tail"
+}
+
+anime_quotes[53] = {
+    "qnumber": 54,
+    "qtext": "I always thought that life was about standing your ground, no matter how strong the current was. But going with the flow isn't so bad after all. As long as it takes you forward.",
+    "qauthor": "Osaki Nana",
+    "qanime": "Nana"
+}
+
+anime_quotes[54] = {
+    "qnumber": 55,
+    "qtext": "Everything happens for a reason. The daily tragedies and misfortunes are all meaningful events, leading toward an ideal conclusion. With that in mind, there probably isn't really any meaningless misfortune.",
+    "qauthor": "Fuwa Aika",
+    "qanime": "Zetsuen No Tempest"
+}
+
+anime_quotes[55] = {
+    "qnumber": 56,
+    "qtext": "If he rips my arms off, I'll kick him to death. If he rips my legs off, I'll bite him to death! If he rips my head off, I'll stare him to death! And if he gouges out my eyes, I'll curse him from beyond the grave!",
+    "qauthor": "Uzumaki Naruto",
+    "qanime": "Naruto Shippuden"
+}
+
+anime_quotes[56] = {
+    "qnumber": 57,
+    "qtext": "I order you to love me!",
+    "qauthor": "Euphemia Li Brittania",
+    "qanime": "Code Geass"
+}
+
+anime_quotes[57] = {
+    "qnumber": 58,
+    "qtext": "We are all farsighted, we give importance to those things that are far from us, while neglecting the things that are close to us...only to realize their value later when they are out-of-reach again...",
+    "qauthor": "Kawamura Sachiko",
+    "qanime": "Nana"
+}
+
+anime_quotes[58] = {
+    "qnumber": 59,
+    "qtext": "I was dead until the moment I met you. I was a powerless corpse pretending to be alive. Living without power, without the ability to change my course, was bound to a slow death.",
+    "qauthor": "Lelouch Lamperouge",
+    "qanime": "Code Geass"
+}
+
+anime_quotes[59] = {
+    "qnumber": 60,
+    "qtext": "You believe that the strong exist to cull the weak. To use them as food. But you are mistaken...The strong exist, not to feed off of the weak, but to protect them!",
+    "qauthor": "Himura Kenshin",
+    "qanime": "Rurouni Kenshin"
+} 
+
+
+
+var quote = $(".quoteText");
+var author = $(".quoteAuthor");
+var anime = $(".anime");
+
+
+$("#nextQuote").on("click", function() {
+    //Random background color + icon color
+    var randomCol = getRandomColor();
+    $(".body, button").css("background-color", randomCol); 
+    $("#icon").css("color", randomCol);
+    
+    renderHTML();
+});
+
+
+
+function renderHTML() {
+    var randomQ = anime_quotes[Math.floor(Math.random() * anime_quotes.length)];
+    quote.text(randomQ.qtext);
+    author.text(randomQ.qauthor);
+    anime.text(randomQ.qanime);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+ 
+
+$("#twitter").on("click", function() {
+  window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent($(".quoteText").text() + " " + $(".quoteAuthor").text() + " - " + $(".anime").text()));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

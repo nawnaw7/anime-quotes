b9caaf8f58cfcc90bf66a363ee00208b1a52802c
nawnaw <sirbu.ioana90@gmail.com>
2017-07-13 18:03:59 +0200

    Mobile responsive
    Made it mobile responsive

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ anime_quotes[1] = {
 
 anime_quotes[2] = {
     "qnumber": 3,
-    "qtext": "There are many types of monsters that scare me: Monsters who cause troubles without showing themselves, monsters who abduct children, monsters who devour dreams, monsters who suck blood... and then, monsters who tell nothing but lies. Lying monsters are a real nuisance: They are much more cunning than others: They pose as humans even though they have no understanding of the human heart; they eat even though they've never experienced hunger; they study even though they have no interest in academics; they seek friendship even though they do not know how to love. If I were to encounter such monsters, I would likely be eaten by them... because in truth, I am that monster.",
+    "qtext": "There are many types of monsters that scare me: Monsters who cause troubles without showing themselves, monsters who abduct children, monsters who devour dreams, monsters who suck blood... and then, monsters who tell nothing but lies.",
     "qauthor": "L",
     "qanime": "Death Note"
 }
